2d36880d9e5403cf50dfb2e35dc6d4f240ca0a87
dksadasjkl <ckdgus6038@naver.com>
2024-04-11 17:40:03 +0900

    Design : 마이페이지 UI 변경

--- src/apis/api/acoountPrincipal.js
+++ src/apis/api/acoountPrincipal.js
@@ -3,3 +3,7 @@ import instance from "../utils/instance"
 export const getPrincipalRequest = async () => {
     return await instance.get("/account/principal");
 }
+
+export const authNicknameEditRequest = async (data) => {
+    return await instance.put("/account/edit/nickname", data);
+}

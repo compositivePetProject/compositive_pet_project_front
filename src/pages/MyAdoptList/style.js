e65c0f357a246779836dca4315276748e7773a55
seowooNote <dkr0210@naver.com>
2024-05-09 16:11:24 +0900

    Refactor: 마이페이지 분양게시판 코드 Refactoring

--- src/pages/MyAdoptList/style.js
+++ src/pages/MyAdoptList/style.js
@@ -4,84 +4,6 @@ export const layout = css`
     box-sizing: border-box;
     display: flex;
     width: 100%;
-    height: 100%;
-`;
-
-export const userInfoBox = css`
-    display: flex;
-    justify-content: center;
-    box-sizing: border-box;
-    margin-top: 45px;
-    width: 300px;
-    height: 100%;
-`;
-
-export const infoBox = css`
-    border-radius: 15px;
-    width: 80%;
-    height: 500px;
-    padding: 0px 24px;
-    border: 1px solid #d7e2eb;
-
-    & > h3 {
-        color: #9b9b9b;
-        font-size: 14px;
-        font-weight: 700;
-        margin: 40px 0px 15px;
-    }
-`;
-
-export const buttons = css`
-    padding: 10px 15px;
-    border-radius: 5px;
-    margin-bottom: 2px;
-    cursor: pointer;
-    &:hover {
-        background-color: #fafafa;
-        color : #0078ff;
-        font-weight: 700;
-    }
-`;
-
-export const buttons2 = css`
-    width: 160px;
-    padding: 10px 20px;
-    border: none;
-    border-radius: 5px;
-    background-color: #e6f2ff;
-    color: #0078ff;
-    text-decoration: none;
-    cursor: pointer;
-`;
-
-export const buttons3 = css`
-    width: 100px;
-    padding: 10px 20px;
-    margin-right: 5px;
-    border: none;
-    border-radius: 5px;
-    background-color: #e6f2ff;
-    color: #0078ff;
-    text-decoration: none;
-    cursor: pointer;
-`;
-
-export const writeButton = css`
-    box-sizing: border-box;
-    margin-top: 50px;
-    border: 1px solid #ccc;
-    padding: 10px;
-    margin-right: 10px;
-    width: 25%;
-    background-color: white;
-    font-weight: 600;
-    cursor: pointer;
-    &:hover {
-        background-color: #fafafa;
-    }
-    &:active {
-        background-color: #eee;
-    }
 `;
 
 export const userDetails = css`
@@ -89,221 +11,53 @@ export const userDetails = css`
     flex-direction: column;
     box-sizing: border-box;
     flex-grow: 1;
+    width: 1000px;
     height: 100%;
     margin-top: 45px;
-    padding: 0px 0px 40px 60px;
+    padding: 0px 80px 80px 40px;
     color: #263747;
-    font-weight: 700;
-    font-size: 18px;
-    border: 1px solid green;
+    font-weight: 600;
+    font-size: 15px;
+
+    & > div:nth-of-type(1) {
+        display: flex;
+        justify-content: space-between;
+    }
 `;
 
 export const title = css`
     font-size: 25px;
     margin-bottom: 24px;
-
 `;
 
-export const box = css`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    border: 1px solid #d7e2eb;
-    border-radius: 15px;
-    padding: 40px;
-    row-gap: 10px;
-    background-color: #ffffff;
-    
-`;
-
-export const imgBox = css`
+export const writeButton = css`
     box-sizing: border-box;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 124px;
-    height: 124px;
-    direction: none;
-    
-`;
-
-export const editimgBox = css`
-    box-sizing: border-box;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 124px;
-    height: 124px;
-    cursor: pointer;
-    position: relative;
-    
-    & > div:nth-of-type(2) {
-        box-sizing: border-box;
-        position: absolute;
-        bottom: 0; 
-        right: 0; 
-        font-size: 30px; 
-    }
-`;
-
-
-
-export const nicknameEditBox = css`
-    width: 100%;
-`;
-
-export const nicknameEdit = css`
-    width: 100%;
-    color: #424242;
-    font-size: 12px;
-`;
-
-export const nickInputEdit = css`
-    flex-grow: 1;
-    padding: 10px 20px;
-    margin-right: 15px;
-    border: 2px solid #e6eef5;
+    border: 1px solid #0e004a24;
     border-radius: 5px;
-    width: 80%;
-    color: #424242;
-    font-size: 12px;
-    background-color: #fbfbfd;
-`;
-
-export const nickCheckButton = css`
-    width: 147px;
-    padding: 10px 20px;
-    border: none;
-    border-radius: 5px;
-    background-color: #e6f2ff;
-    color: #0078ff;
-    text-decoration: none;
-    cursor: pointer;
-`;
-
-
-export const profileImg = css`
-    box-sizing: border-box;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border: 1px solid #dbdbdb;
-    border-radius: 50%;
-    width: 100%;
-    height: 100%;
-    overflow: hidden;
-    & > img {
-        height: 100%;   
-    }  
-`;
-
-export const passwordBox = css`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    border: 1px solid #d7e2eb;
-    border-radius: 15px;
-    padding: 40px;
-    row-gap: 10px;
-    background-color: #ffffff;
-    
-`
-
-export const boardListHeader = css`
-    width: 100%;
-    display: flex;
-    margin: 5px 0px 0px 0px;
-    box-sizing: border-box;
-
-    & > div:nth-of-type(1) {
-        border-top-left-radius: 5px;
-        width: 5%;
-        border-right: 1px solid #eee;
-    }
-    & > div:nth-of-type(2) {
-        width: 40%;
-        border-right: 1px solid #eee;
-    }
-    & > div:nth-of-type(3) {
-        width: 25%;
-        border-right: 1px solid #eee;
-    }
-    & > div:nth-of-type(4) {
-        width: 30%;
-        border-right: 1px solid #eee;
-        border-top-right-radius: 5px;
-    }
-`;
-
-export const label = css`
-    box-sizing: border-box;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 40px;
-    font-size: 14px;
+    padding: 0px 15px;
+    background-color: white;
     font-weight: 600;
-    background-color: #00005cff;
-    color: #eee;
-    cursor: default;
+    height: 40px;
+    cursor: pointer;
+    &:hover {
+        border: 1px solid #2400c4cb;
+    }
+
+    &:active {
+        background-color: #00005cff;
+        color: #eeeeee;
+    }
 `;
+
 
 export const boardListItem = css`
+    border: 1px solid #d7e2eb;
+    height: 500px;
+    padding: 25px 25px 25px 30px;
+    overflow: auto;
+    box-sizing: border-box;
+    gap: 50px;
     flex-wrap: wrap;
-    display: flex; 
-`;
-
-export const rowData = css`
-    width: 100%;
     display: flex;
-
-    & > div:nth-of-type(1) {
-        width: 5%;
-        border-right: 1px solid #eee;
-    }
-    & > div:nth-of-type(2) {
-        width: 40%;
-        border-right: 1px solid #eee;
-    }
-    & > div:nth-of-type(3) {
-        width: 25%;
-        border-right: 1px solid #eee;
-    }
-    & > div:nth-of-type(4) {
-        width: 30%;
-        border-right: 1px solid #eee;
-    }
+    border-radius: 8px;
 `;
-
-export const labelData = css`
-    box-sizing: border-box;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border: 1px solid #0e004a24;
-    font-size: 14px;
-    font-weight: 600;
-    height: 40px;
-    cursor: default;
-`;
-
-export const likeHeart = css`
-    width: 25px;
-    height: 25px;
-`
-
-export const status = css`
-    box-sizing: border-box;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-`
-
-
-
-
-
-
-

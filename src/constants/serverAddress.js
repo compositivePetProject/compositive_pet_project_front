149424205f99f444f6d1065c64275875bd6ca133
seowooNote <dkr0210@naver.com>
2024-05-17 11:51:55 +0900

    Style: index.html 파일 수정

--- src/constants/serverAddress.js
+++ src/constants/serverAddress.js
@@ -2,5 +2,5 @@ export const LOCAL_ADDRESS = 'localhost:8080';
 export const DEPLOYT_ADDRESS = '3.39.39.112';
 
 export default function getServerAddress() {
-    return LOCAL_ADDRESS;
+    return DEPLOYT_ADDRESS;
 }

a7bd50e10bf037e5e0a116440a20bd5fb11c71be
seowooNote <dkr0210@naver.com>
2024-05-17 15:42:52 +0900

    Style: 수정

--- src/apis/utils/instance.js
+++ src/apis/utils/instance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import getServerAddress from "../../constants/serverAddress";
 
 export const instance = axios.create({
-    baseURL: getServerAddress(),
+    baseURL: "http://" + getServerAddress(),
     headers: {
         Authorization: "Bearer " + localStorage.getItem("AccessToken")
     }
